70db7ae7c8c7d40a8045258b5057a8f25a0fcf23
nikola-vujkovic <vujkovic.software@gmail.com>
2022-01-20 20:13:02 +0100

    Added portal constants

---
+++ src/constants/portalConstants.js
@@ -0,0 +1,30 @@
+export default {
+  space: {
+    portalTitle: 'Put kroz svemir',
+    portalSource: require('../videos/space.mp4'),
+    portalId: 'space',
+  },
+  bloodflow: {
+    portalTitle: 'Mikrosvijet covjeka',
+    portalSource: require('../videos/bloodflow.mp4'),
+    portalId: 'bloodflow',
+  },
+  lions: {
+    portalTitle: 'Susret sa lavovima',
+    portalSource: require('../videos/lions.mp4'),
+    portalId: 'lions',
+  },
+  asteroid: {
+    portalTitle: 'Napad asteroida',
+    portalSource: require('../videos/asteroid.mp4'),
+    portalId: 'asteroid',
+  },
+  underwater: {
+    portalTitle: 'Okeanske dubine',
+    portalSource: require('../videos/underwater.mp4'),
+    portalId: 'underwater',
+  },
+};
+
+// How to turn a list of object properties into an array
+// https://stackoverflow.com/questions/46153975/using-flat-list-with-objectmap
